fa8c6bd9a45169e77cf07942aeca4ca761414571
OscarYn <ofernandezg3@upao.edu.pe>
2023-10-14 09:12:47 -0500

    first commit

---
+++ src/main/java/edu/pe/upao/localboost/LocalBoostApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.upao.localboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalBoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalBoostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/upao/localboost/LocalBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.upao.localboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalBoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
